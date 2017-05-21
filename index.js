637e39bc90dded53142e28ab61839c702c6a6d54
shilx <haseeer@gmail.com>
2017-05-21 21:56:09 +0800

    '优化棋子判断，自动切换执子方'

--- index.js
+++ index.js
@@ -81,11 +81,26 @@ var board = {
         return true
     },
     moveChessman: function (code) {
-        var targetClass = '',
+        var targetClass,
+            targetName,
+            targetNum,
             afertLocation = { x: 0, y: 0 },
             beforeLocation = { x: 0, y: 0 }
-        //判断棋子名
+        //判断是否同一纵向上
         switch (code[0]) {
+            case '前':
+                targetName = code[1]
+                targetNum = 0
+                break
+            case '后':
+                targetName = code[1]
+                targetNum = 1
+                break
+            default:
+                targetName = code[0]
+        }
+        //判断棋子名
+        switch (targetName) {
             case '车':
             case '車':
                 targetClass = 'ju'
@@ -93,14 +108,17 @@ var board = {
             case '马':
             case '馬':
                 targetClass = 'ma'
+                addY = 2
                 break
             case '象':
             case '相':
                 targetClass = 'xiang'
+                addY = 3
                 break
             case '士':
             case '仕':
                 targetClass = 'shi'
+                addY = 1
                 break
             case '将':
             case '帥':
@@ -120,18 +138,31 @@ var board = {
         }
         //结合执子方及位置选中具体棋子
         //象棋行棋步骤参考 https://zhidao.baidu.com/question/3673556.html
-        //需要注意的是：目前未判断棋子在同一竖线上的情况
         //2017年5月17日16:00:38
         //象棋规则参考 http://www.xqbase.com/protocol/rule.htm
         var $chessman = $('.' + targetClass + '.' + this.player)
-        $chessman.each(function () {
-            if (board.convertNumber($(this).attr('location').split('-')[0]) == board.convertNumber(code[1])) {
-                $chessman = $(this);
-                return false
+        console.log('.' + targetClass + '.' + this.player)
+        //判断两个棋子是否在同一竖线
+        if (targetNum !== undefined) {
+            var max = parseInt($chessman.eq(0).attr('location')[2]),
+                min = parseInt($chessman.eq(1).attr('location')[2]),
+                x = $chessman.eq(0).attr('location')[0]
+            if (max > min) {
+                $chessman = $('.' + this.player + '[location=' + x + '-' + (targetNum == 0 ? max : min) + ']')
             } else {
-                $chessman = undefined
+                $chessman = $('.' + this.player + '[location=' + x + '-' + (targetNum == 0 ? min : max) + ']')
             }
-        })
+        } else {
+            $chessman.each(function (index, domEl) {
+                //以用户输入的横坐标判断
+                if (board.convertNumber($(this).attr('location').split('-')[0]) == board.convertNumber(code[1])) {
+                    $chessman = $(this)
+                    return false
+                } else {
+                    $chessman = undefined
+                }
+            })
+        }
         //未找到排错
         if ($chessman === undefined) {
             this.showText('未找到' + code[0] + code[1] + "，请检查！")
@@ -140,35 +171,118 @@ var board = {
         console.info('准备移动', code[0] + code[1])
         // $chessman.removeClass('red black')
         //计算落子点坐标
-        beforeLocation.x = parseInt($chessman.attr('location').split('-')[0])
-        beforeLocation.y = parseInt($chessman.attr('location').split('-')[1])
+        beforeLocation = {
+            x: parseInt($chessman.attr('location').split('-')[0]),
+            y: parseInt($chessman.attr('location').split('-')[1])
+        }
         console.info('移动前坐标', beforeLocation)
         //未判断马象士
-        switch (code[2]) {
-            //平：Y不变，直接改X
-            case '平':
-                afertLocation.x = this.convertNumber(code[3])
-                afertLocation.y = beforeLocation.y
+        var codeMove = this.convertNumber(code[3])
+        switch (targetClass) {
+            case 'ma':
+                switch (code[2]) {
+                    case '平':
+                        //马无平
+                        this.showText('马无平')
+                        break
+                    case '进':
+                        //判断差值，x+2则y+1，x+1则y+2,x-2则y+1，x-1则y+2
+                        afertLocation.x = codeMove
+                        if ((codeMove - beforeLocation.x) == 2 || (beforeLocation.x - codeMove) == 2) {
+                            afertLocation.y = beforeLocation.y + 1
+                        } else if ((codeMove - beforeLocation.x) == 1 || (beforeLocation.x - codeMove) == 1) {
+                            afertLocation.y = beforeLocation.y + 2
+                        } else {
+                            this.showText('请注意，马走“日”')
+                            return
+                        }
+                        break
+                    case '退':
+                        //判断差值，x+2则y+1，x+1则y+2,x-2则y+1，x-1则y+2
+                        afertLocation.x = codeMove
+                        if ((codeMove - beforeLocation.x) == 2 || (beforeLocation.x - codeMove) == 2) {
+                            afertLocation.y = beforeLocation.y - 1
+                        } else if ((codeMove - beforeLocation.x) == 1 || (beforeLocation.x - codeMove) == 1) {
+                            afertLocation.y = beforeLocation.y - 2
+                        } else {
+                            this.showText('请注意，马走“日”')
+                            return
+                        }
+                        break
+                    default:
+                        this.showText('输入了奇怪的语句：' + code[2])
+                }
                 break
-            //进/退：X不变，加/减Y
-            case '进':
-                afertLocation.x = beforeLocation.x
-                afertLocation.y = (parseInt(beforeLocation.y) + this.convertNumber(code[3]))
+            case 'xiang':
+                switch (code[2]) {
+                    case '平':
+                        this.showText(code[0] + '无平')
+                        break
+                    case '进':
+                        afertLocation = {
+                            x: codeMove,
+                            y: beforeLocation.y + 2
+                        }
+                        break
+                    case '退':
+                        afertLocation = {
+                            x: codeMove,
+                            y: beforeLocation.y - 2
+                        }
+                        break
+                    default:
+                }
                 break
-            case '退':
-                afertLocation.x = beforeLocation.x
-                afertLocation.y = (parseInt(beforeLocation.y) - this.convertNumber(code[3]))
+            case 'shi':
+                switch (code[2]) {
+                    case '平':
+                        this.showText(code[0] + '无平')
+                        break
+                    case '进':
+                        afertLocation = {
+                            x: codeMove,
+                            y: beforeLocation.y + 1
+                        }
+                        break
+                    case '退':
+                        afertLocation = {
+                            x: codeMove,
+                            y: beforeLocation.y - 1
+                        }
+                        break
+                    default:
+                }
                 break
             default:
-                this.showText('走法只能为进、退、平，请检查')
-                return
+                switch (code[2]) {
+                    //平：Y不变，直接改X
+                    case '平':
+                        afertLocation.x = codeMove
+                        afertLocation.y = beforeLocation.y
+                        break
+                    //进/退：X不变，加/减Y
+                    case '进':
+                        afertLocation.x = beforeLocation.x
+                        afertLocation.y = (parseInt(beforeLocation.y) + codeMove)
+                        break
+                    case '退':
+                        afertLocation.x = beforeLocation.x
+                        afertLocation.y = (parseInt(beforeLocation.y) - codeMove)
+                        break
+                    default:
+                        this.showText('走法只能为进、退、平，请检查')
+                        return
+                }
         }
         console.info('移动后坐标', afertLocation)
 
         /**
          * 判断移动后坐标是否超出棋盘
-         * 这里需判断棋子行进规则，如车行进路线不能有棋子阻挡
          */
+        if (afertLocation.x > 9 || afertLocation.y > 10) {
+            this.showText('落子点超出棋谱，请检查！')
+            return
+        }
         //判断落子点是否存在棋子
         var $targetChessman = $('.chessman[location=' + afertLocation.x + '-' + afertLocation.y + ']')
         if ($targetChessman.length > 0) {
@@ -200,6 +314,15 @@ $(function () {
         for (var i = 0; i < 8 * 4; i++) {
             html += function (index) {
                 switch (index) {
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                        return '<div class="block mark-num"></div>'
                     case 8:
                         return '<div class="block mark-rb"></div>'
                     case 9:
@@ -242,11 +365,12 @@ $(function () {
     })
     //
     $('#go').click(function () {
-        board.player = $(".player:checked").val()
-        $('#codes a').each(function(){
-            if($(this).hasClass('disabled')) return
+        $('#codes a').each(function () {
+            board.player = $(".player:checked").val()
+            if ($(this).hasClass('disabled')) return
             $(this).addClass('active').prevAll('.active').removeClass('active')
             board.moveChessman($(this).text())
+            $(".player:checked").parent().siblings().click();
         })
         // $.trim($('#code').val()).split(/\n/)
     })
